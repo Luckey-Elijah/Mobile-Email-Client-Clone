56a7cf15c78f16300a43d24acacd7d7cce97e73b
Elijah Luckey <luckeyelijah112@gmail.com>
2020-05-17 17:03:32 -0400

    gettter for date and time

--- lib/data/email_message.dart
+++ lib/data/email_message.dart
@@ -16,4 +16,44 @@ class EmailMessage {
     this.timeStamp,
     this.starred,
   });
+
+  get date {
+    String readableDateTime;
+    String weekdayString = _dayOfTheWeek(timeStamp.weekday);
+    readableDateTime =
+        '$weekdayString, ${timeStamp.month}/${timeStamp.day}/${timeStamp.year}';
+    return readableDateTime;
+  }
+
+  get time {
+    return '${timeStamp.hour}:${timeStamp.minute}';
+  }
+
+  String _dayOfTheWeek(int dayIndex) {
+    switch (dayIndex) {
+      case 1:
+        return 'Monday';
+        break;
+      case 2:
+        return 'Tuesday';
+        break;
+      case 3:
+        return 'Wednesday';
+        break;
+      case 4:
+        return 'Thursday';
+        break;
+      case 5:
+        return 'Friday';
+        break;
+      case 6:
+        return 'Saturday';
+        break;
+      case 7:
+        return 'Sunday';
+        break;
+      default:
+        return 'Monday';
+    }
+  }
 }
